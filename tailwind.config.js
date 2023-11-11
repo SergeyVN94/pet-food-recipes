80477bfc909f955de5a5370a6a71af58cb7da0d3
SergeyVN94 <blakdgak@yandex.ru>
2023-11-11 21:34:00 +0300

    Настроил форматирование

--- tailwind.config.js
+++ tailwind.config.js
@@ -90,22 +90,17 @@ const palette = {
     95: '#F5EEFA',
     99: '#FFFBFE',
     100: '#FFFFFF',
-  }
+  },
 };
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
+  darkMode: ['class'],
+  content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
   theme: {
     container: {
       center: true,
-      padding: "24px",
+      padding: '24px',
     },
     fontFamily: {
       sans: ['var(--font-roboto)', 'sans-serif'],
@@ -173,8 +168,16 @@ module.exports = {
         'elevation-3': '0px 1px 3px 0px rgba(0, 0, 0, 0.30), 0px 4px 8px 3px rgba(0, 0, 0, 0.15)',
         'elevation-4': '0px 2px 3px 0px rgba(0, 0, 0, 0.30), 0px 6px 10px 4px rgba(0, 0, 0, 0.15)',
         'elevation-5': '0px 4px 4px 0px rgba(0, 0, 0, 0.30), 0px 8px 12px 6px rgba(0, 0, 0, 0.15)',
-      }
+      },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-}
+  plugins: [
+    require('tailwindcss-animate'),
+    /** @type {import('tailwindcss/types/config').PluginCreator} */
+    ({ addComponents }) => {
+      addComponents({
+        '.display-l': '@apply font-sans text-black text-6xl font-normal leading-10',
+      });
+    },
+  ],
+};
