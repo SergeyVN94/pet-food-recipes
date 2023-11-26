d102cacd15c85782a17f498d75f339de6d8905f9
SergeyVN94 <blakdgak@yandex.ru>
2023-11-26 22:31:29 +0300

    Пофиксил компонент ButtonIcon

--- src/components/ui/ButtonIcon/ButtonIcon.tsx
+++ src/components/ui/ButtonIcon/ButtonIcon.tsx
@@ -2,14 +2,14 @@ import React, { ButtonHTMLAttributes, ReactComponentElement, cloneElement } from
 import { cva, type VariantProps } from 'class-variance-authority';
 
 const variants = cva(
-  'p-2 border-transparent rounded-full transition-all block group-disabled:cursor-not-allowed group-disabled:opacity-40',
+  'p-2 border-transparent rounded-full transition-all block group-disabled/button-icon:cursor-not-allowed group-disabled/button-icon:opacity-40',
   {
     variants: {
       variant: {
         standard:
-          'text-on-surface-var group-hover:bg-on-surface-var/[0.08] group-focus:bg-on-surface-var/[0.12] group-active:bg-on-surface-var/[0.12] group-disabled:bg-transparent group-disabled:text-on-surface',
+          'text-on-surface-var group-hover/button-icon:bg-on-surface-var/[0.08] group-focus/button-icon:bg-on-surface-var/[0.12] group-active/button-icon:bg-on-surface-var/[0.12] group-disabled/button-icon:bg-transparent group-disabled/button-icon:text-on-surface',
         filled:
-          'text-primary bg-surf-cont-highest/70 group-hover:bg-surf-cont-highest/90 group-focus:bg-surf-cont-highest/95 group-active:bg-surf-cont-highest/95 group-disabled:bg-on-surface group-disabled:text-on-surface',
+          'text-primary bg-surf-cont-highest/70 group-hover/button-icon:bg-surf-cont-highest/90 group-focus/button-icon:bg-surf-cont-highest/95 group-active/button-icon:bg-surf-cont-highest/95 group-disabled/button-icon:bg-on-surface group-disabled/button-icon:text-on-surface',
       },
     },
     defaultVariants: {
@@ -24,7 +24,7 @@ type ButtonIconProps = {
   VariantProps<typeof variants>;
 
 const ButtonIcon = ({ children, className, variant, ...other }: ButtonIconProps) => (
-  <button className="p-1 group" {...other}>
+  <button className="p-1 group/button-icon outline-none border-none bg-transparent" {...other}>
     <span className={variants({ variant })}>
       {cloneElement(children, {
         width: 24,
