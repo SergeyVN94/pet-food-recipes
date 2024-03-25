a677b903b2bdc21001d73bb57242f6d1dbc5e822
SergeyVN94 <blakdgak@yandex.ru>
2024-03-25 15:58:01 +0300

    Доработка компонентов Select и Input

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ChangeEvent, ChangeEventHandler, TextareaHTMLAttributes, forwardRef, useEffect, useRef } from 'react';
+import React from 'react';
 import { useFormContext } from 'react-hook-form';
 import { mergeRefs } from 'react-merge-refs';
 import { ButtonIcon } from '../ButtonIcon';
@@ -12,108 +12,132 @@ type InputProps = {
   label?: string;
   subText?: string;
   className?: string;
-  onChange?: (ev: ChangeEvent<HTMLInputElement>, value: string) => void;
+  isFocus?: boolean;
+  iconLeft?: React.JSX.Element;
+  iconRight?: React.JSX.Element;
+  onChange?: (ev: React.ChangeEvent<HTMLInputElement>, value: string) => void;
   onClear?: () => void;
-} & Omit<TextareaHTMLAttributes<HTMLInputElement>, 'onChange'> &
+} & Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> &
   Omit<InputVariantProps, 'focus'>;
 
-const Input = forwardRef<HTMLInputElement, InputProps>(({ variant, label, subText, onChange, onClear, className = '', ...other }, ref) => {
-  const localRef = useRef<HTMLInputElement>(null);
-  const labelRef = useRef<HTMLSpanElement>(null);
-  const buttonClearRef = useRef<HTMLButtonElement>(null);
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ variant, label, subText, onChange, onClear, className = '', isFocus, iconRight, iconLeft, ...other }, ref) => {
+    const localRef = React.useRef<HTMLInputElement>(null);
+    const labelRef = React.useRef<HTMLSpanElement>(null);
+    const buttonClearRef = React.useRef<HTMLButtonElement>(null);
 
-  const handleChange: ChangeEventHandler<HTMLInputElement> = (ev) => {
-    const nextValue = ev.target.value;
+    const handleChange: React.ChangeEventHandler<HTMLInputElement> = (ev) => {
+      const nextValue = ev.target.value;
 
-    if (buttonClearRef.current) {
-      buttonClearRef.current.style.display = nextValue ? '' : 'none';
-    }
+      if (onChange) {
+        onChange(ev, nextValue);
+      }
 
-    if (onChange) {
-      onChange(ev, nextValue);
-    }
-  };
-
-  const handleRootClick = () => {
-    localRef.current?.focus();
-  };
-
-  const handleClear = () => {
-    if (localRef.current) {
-      labelRef.current!.setAttribute('data-focus', 'false');
-      localRef.current.style.height = '';
-    }
-
-    if (onClear) {
-      onClear();
-    }
-  };
-
-  useEffect(() => {
-    const handleChange = () => {
-      if (localRef.current) {
-        labelRef.current?.setAttribute('data-focus', (!!localRef.current.value).toString());
+      if (buttonClearRef.current && localRef.current) {
+        buttonClearRef.current.style.opacity = localRef.current.value || other.value ? '1' : '0';
       }
     };
 
-    const inputEl = localRef.current;
-    inputEl?.addEventListener('change', handleChange);
-
-    return () => {
-      inputEl?.removeEventListener('change', handleChange);
+    const handleRootClick = () => {
+      localRef.current?.focus();
     };
-  }, []);
 
-  useEffect(() => {
-    labelRef.current?.setAttribute('data-focus', (!!(other.value || other.placeholder)).toString());
+    const handleClear = () => {
+      if (localRef.current) {
+        labelRef.current?.setAttribute('data-focus', 'false');
+        localRef.current.style.height = '';
+      }
 
-    if (buttonClearRef.current) {
-      buttonClearRef.current.style.display = other.value ? '' : 'none';
-    }
-  }, [other.value, other.placeholder]);
+      if (buttonClearRef.current) {
+        buttonClearRef.current.style.opacity = '0';
+      }
 
-  return (
-    <div className={className}>
-      <div className={wrapVariants({ variant })} onClick={handleRootClick}>
-        <label className="flex-1 relative pt-1">
-          <input
-            className="peer w-full outline-none bg-transparent body-l text-on-surface pr-4 mt-4"
-            {...other}
-            onChange={handleChange}
-            ref={mergeRefs([ref, localRef])}
-          />
-          {label && (
-            <span className={labelVariants({ variant })} ref={labelRef}>
-              {label}
-            </span>
+      if (onClear) {
+        onClear();
+      }
+    };
+
+    React.useEffect(() => {
+      const handleChange = () => {
+        if (localRef.current) {
+          labelRef.current?.setAttribute('data-focus', Boolean(localRef.current.value).toString());
+        }
+      };
+
+      const inputEl = localRef.current;
+      inputEl?.addEventListener('change', handleChange);
+
+      return () => {
+        inputEl?.removeEventListener('change', handleChange);
+      };
+    }, []);
+
+    return (
+      <div className={className}>
+        <div className={wrapVariants({ variant })} onClick={handleRootClick}>
+          {iconLeft &&
+            React.cloneElement(iconLeft, {
+              width: 48,
+              height: 48,
+              className: 'p-3',
+            })}
+          <label className="flex-1 relative pt-1">
+            <input
+              className="peer w-full outline-none bg-transparent body-l text-on-surface pr-4 mt-4 data-[with-label=false]:mt-2"
+              {...other}
+              data-with-label={String(Boolean(label))}
+              onChange={other.readOnly ? undefined : handleChange}
+              ref={mergeRefs([ref, localRef])}
+            />
+            {label && (
+              <span
+                className={labelVariants({ variant })}
+                ref={labelRef}
+                data-force-focus={Boolean(other.value || other.placeholder || isFocus)}
+              >
+                {label}
+              </span>
+            )}
+          </label>
+          {onClear && (
+            <ButtonIcon className="opacity-0" onClick={handleClear} type="button" ref={buttonClearRef}>
+              <IconCancel />
+            </ButtonIcon>
           )}
-        </label>
-        <ButtonIcon onClick={handleClear} type="button" ref={buttonClearRef}>
-          <IconCancel />
-        </ButtonIcon>
+          {iconRight &&
+            React.cloneElement(iconRight, {
+              width: 48,
+              height: 48,
+              className: 'p-3',
+            })}
+        </div>
+        {subText && <p className="pt-1 px-4 body-s text-on-surface-var">{subText}</p>}
       </div>
-      {subText && <p className="pt-1 px-4 body-s text-on-surface-var">{subText}</p>}
-    </div>
-  );
-});
+    );
+  },
+);
 Input.displayName = 'Input';
 
 type InputControlledProps = Omit<InputProps, 'onChange' | 'value' | 'onBlur' | 'onFocus' | 'name'> & {
   name: string;
+  isClearable?: boolean;
 };
 
-export const InputControlled = (props: InputControlledProps) => {
+export const InputControlled = ({ isClearable = true, ...other }: InputControlledProps) => {
   const methods = useFormContext();
 
-  const handleClear = () => {
-    methods.resetField(props.name);
+  const handleClear =
+    other.onClear || isClearable
+      ? () => {
+          methods.resetField(other.name);
 
-    if (props.onClear) {
-      props.onClear();
-    }
-  };
+          if (other.onClear) {
+            other.onClear();
+          }
+        }
+      : undefined;
 
-  return <Input {...methods.register(props.name)} {...props} onClear={handleClear} />;
+  return <Input {...methods.register(other.name)} {...other} onClear={handleClear} />;
 };
 
 export default Input;
---
+++ src/components/ui/Select/Select.tsx
@@ -0,0 +1,167 @@
+'use client';
+
+import React, { useEffect } from 'react';
+import * as SelectPrimitives from '@radix-ui/react-select';
+import { useVirtualizer } from '@tanstack/react-virtual';
+import { useFormContext, Controller } from 'react-hook-form';
+import debounce from 'lodash-es/debounce';
+
+import { Input } from '../Input';
+import { cn } from '@/lib/utils';
+import { IconArrowLeft, IconSearch } from '@/assets/icons';
+
+export type SelectItem = {
+  id: string;
+  label: string;
+};
+
+type SelectProps = {
+  ariaLabel?: string;
+  label?: string;
+  className?: string;
+  items: SelectItem[];
+  name: string;
+  value?: SelectItem['id'];
+  error?: string;
+  onChange: (id: SelectItem['id']) => void;
+  onFocus?: () => void;
+  onBlur?: () => void;
+};
+
+const Select = ({ ariaLabel, items, value, error, label, name, className, onChange, onBlur, onFocus }: SelectProps) => {
+  const selectedItemLabel = React.useMemo(() => items.find((i) => i.id === value)?.label, [items, value]);
+  const [inputValue, setInputValue] = React.useState(selectedItemLabel ?? '');
+  const [filter, setFilter] = React.useState(selectedItemLabel ?? '');
+  const setFilterDebounced = React.useMemo(() => debounce(setFilter, 250), [setFilter]);
+  const filteredItems = React.useMemo(() => {
+    const normalizedFilter = filter.trim().toLowerCase();
+    return normalizedFilter.length > 0 ? items.filter((i) => i.label.toLowerCase().includes(normalizedFilter)) : items;
+  }, [filter, items]);
+  const searchInputRef = React.useRef<HTMLInputElement>(null);
+  const [isOpen, setIsOpen] = React.useState(false);
+  const itemsParentRef = React.useRef<HTMLDivElement>(null);
+
+  const handleInputChange = (_: React.ChangeEvent<HTMLInputElement>, value: string) => {
+    setInputValue(value);
+    setFilterDebounced(value);
+  };
+
+  const rowVirtualizer = useVirtualizer({
+    count: filteredItems.length,
+    getScrollElement: () => itemsParentRef.current,
+    estimateSize: () => 56,
+  });
+
+  useEffect(() => {
+    if (isOpen) {
+      searchInputRef.current?.focus();
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    setInputValue(selectedItemLabel ?? '');
+    setFilter(selectedItemLabel ?? '');
+  }, [selectedItemLabel]);
+
+  return (
+    <SelectPrimitives.Root onValueChange={onChange} onOpenChange={setIsOpen} open={isOpen} name={name} value={value}>
+      <SelectPrimitives.Trigger
+        className={cn(
+          'outline-none w-full cursor-pointer py-1 pl-4 relative bg-surf-cont-highest rounded-t min-h-[3.5rem] after:absolute after:block after:w-full after:bottom-0 after:left-0 after:h-[1px] after:bg-on-surface  hover:bg-on-surface/10 focus:after:h-0.5 focus:after:bg-primary min-w-[10rem]',
+          className,
+        )}
+        aria-label={ariaLabel}
+        onFocus={onFocus}
+        onBlur={onBlur}
+      >
+        {label && (
+          <span
+            data-focus={isOpen || !!value?.length}
+            className='text-on-surface-var hover:text-primary !body-l whitespace-nowrap absolute top-4 left-4 group-hover:text-on-surface-var peer-focus:text-primary peer-focus:top-1 peer-focus:body-s peer-focus:text-xs peer-placeholder-shown:text-primary peer-placeholder-shown:top-1 peer-placeholder-shown:body-s peer-placeholder-shown:text-xs data-[focus="true"]:text-primary data-[focus="true"]:top-1 data-[focus="true"]:body-s data-[focus="true"]:text-xs transition-all overflow-ellipsis'
+          >
+            {label}
+          </span>
+        )}
+        <span className="flex items-center w-full">
+          <span className="body-l data-[label='true']:pt-3 whitespace-nowrap overflow-ellipsis" data-label={!!label}>
+            {selectedItemLabel}
+          </span>
+          <IconArrowLeft
+            className="ml-auto mr-3 -rotate-90 data-[open='true']:rotate-90 transition-all"
+            data-open={isOpen}
+            width={24}
+            height={24}
+          />
+        </span>
+      </SelectPrimitives.Trigger>
+      <SelectPrimitives.Portal>
+        <SelectPrimitives.Content sideOffset={4} alignOffset={0} position="popper" autoFocus={false}>
+          <SelectPrimitives.Viewport className="rounded-md bg-surf-cont flex flex-col w-full shadow-elevation-2" autoFocus={false}>
+            <Input
+              value={inputValue}
+              onChange={handleInputChange}
+              name={`select-search-${name}`}
+              ref={searchInputRef}
+              iconLeft={<IconSearch />}
+            />
+            {/* 21rem == 6 items */}
+            <div className="max-h-[21rem] overflow-y-auto" ref={itemsParentRef}>
+              <div className="w-full relative" style={{ height: `${rowVirtualizer.getTotalSize()}px` }}>
+                {rowVirtualizer.getVirtualItems().map((virtualItem) => (
+                  <div
+                    key={virtualItem.key}
+                    style={{
+                      position: 'absolute',
+                      top: 0,
+                      left: 0,
+                      width: '100%',
+                      height: `${virtualItem.size}px`,
+                      transform: `translateY(${virtualItem.start}px)`,
+                    }}
+                  >
+                    <SelectPrimitives.Item
+                      key={filteredItems[virtualItem.index].id}
+                      className="px-3 py-4 w-full text-left hover:bg-surf-cont-highest transition-all body-l max-w-xs text-ellipsis select-none cursor-pointer focus:bg-surf-cont-high outline-none"
+                      value={filteredItems[virtualItem.index].id}
+                    >
+                      <SelectPrimitives.ItemText>{filteredItems[virtualItem.index].label}</SelectPrimitives.ItemText>
+                    </SelectPrimitives.Item>
+                  </div>
+                ))}
+              </div>
+            </div>
+            {filteredItems.length === 0 && (
+              <p className="px-3 py-4 w-full text-left hover:bg-surf-cont-highest transition-all body-l max-w-xs text-ellipsis select-none cursor-default focus:bg-surf-cont-high outline-none">
+                Ничего не найдено
+              </p>
+            )}
+          </SelectPrimitives.Viewport>
+        </SelectPrimitives.Content>
+      </SelectPrimitives.Portal>
+    </SelectPrimitives.Root>
+  );
+};
+
+type SelectControlledProps = Omit<SelectProps, 'onChange' | 'value'>;
+
+export const SelectControlled = ({ ...other }: SelectControlledProps) => {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      control={control}
+      name={other.name}
+      render={({ field }) => {
+        const handleChange = (value: string) => {
+          if (value.length > 0) {
+            field.onChange(value);
+          }
+        };
+
+        return <Select {...other} onChange={handleChange} value={field.value} onBlur={field.onBlur} />;
+      }}
+    />
+  );
+};
+
+export default Select;
